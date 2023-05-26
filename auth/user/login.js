26185633349c29255e720478e676e93daef2d951
Dhananjay Dogne <111991487+DhananjayDogne@users.noreply.github.com>
2023-05-26 16:19:47 +0530

    Adding all the other files

--- auth/user/login.js
+++ auth/user/login.js
@@ -1,15 +1,26 @@
 import { config } from 'dotenv'
 import axios from 'axios'
-import { token_path } from '../routes.js'
+import { token_path_login } from '../routes.js'
 
 config();
 
 const { BASE_URL } = process.env;
 
 export const secretLogin = async ({ email, password }) => {
-    const { data } = await axios.post(`${BASE_URL}${token_path}`, {
-        "email": email,
-        "password": password,
-    })
-    return data
+    if(email =="" || password ==""){
+        return "Please fill Email And Password Correctly...";
+    }else{
+        const {data}  = await axios.post(`${BASE_URL}${token_path_login}`, {
+            "email": email,
+            "password": password,
+        }).catch(err => err)
+        if(data){
+                  return data.access
+             
+        }else{
+            return "Wrong details ! Please fill correctly" ;
+        }
+    }
+   
+   
 };
